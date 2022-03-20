86ee128ebe23ad3de64fa5d18400ea7b2eb70d28
tarkan <tarknsahin@gmail.com>
2022-03-20 12:23:10 +0100

    second work

---
+++ second.py
@@ -0,0 +1,8 @@
+n1=int(input('sayi giriniz'))
+n2=int(input('sayi giriniz'))
+n3=int(input('sayi giriniz'))
+result1=n1 * n2 * n3
+print(result1)
+#print("your first number {}", 'second number{} and last number{}".format(n1,n2,n3))
+print(f"the result of multiplication of {n1} {n2} and {n3} equqls to (result1)"
+print("the result of multiplication {} {} and {} equals to {}".format(n1,n2,n3,result1))
